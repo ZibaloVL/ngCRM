3fefe962d92862fc14a30218c757461e117d68ed
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2020-05-13 06:25:43 +0300

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('./app')
+const port = process.env.PORT || 5000
+
+
+app.get ('/', ( req, res ) => {
+    res.status(200).json({ message: 'worc'})
+} )
+
+app.listen ( 5000, 
+    () => {console.log(`server start in port  ${port}`)
+})
+
