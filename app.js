6500d9f7e34fe0580a93f28cbaf6f853d7055db5
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2020-05-13 07:12:07 +0300

    end Blok2 less4

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require ('express')
 
+//  routers
+const authRoutes = require('./routes/auth')
+// end routers
 const app = express()
+
+// conect routers
+    app.use('/api/auth', authRoutes)
+// end con routers
 
 
 module.exports = app
