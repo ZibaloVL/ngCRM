26c902d944c4718b0a0e807e1219320ba4ec5c3d
ZibaloV <fotoroom.md@gmail.com>
2020-05-20 05:55:35 +0300

    end bloc4 les 29

---
+++ middleware/upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer') // for load file in storage
+const moment = require('moment')
+
+const storage = multer.diskStorage({
+  destination( req, file, cb ) {
+    cb( null, 'uploads/')
+  },
+  filename( req, file, cb ) {
+    const date = moment().format('DDMMYYYY-HHmmss_SSS')
+    cb( null, `${date}-${file.originalname}`)
+  } 
+})
+
+const fileFilter = ( req, file, cb) => {
+  console.log ('file_', file ) // mimetype???
+  if( file.mimetype === 'img/png' || file.mimetype === 'img/jpg' ) {
+    cb( null, true )
+  } else {
+    cb ( null, false )
+  }
+}
+
+const limits = {
+  fileSize = 1024*1024*5
+}
+
+module.exports = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: limits
+})
