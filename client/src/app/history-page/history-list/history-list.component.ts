68446b5ce373adaa421aba90ea244b7b1f72a47e
ZibaloV <fotoroom.md@gmail.com>
2020-06-23 07:12:13 +0300

    end 68

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { Order } from 'src/app/shared/interfaces';
+import { MaterialInstance, MaterialService } from 'src/app/shared/classes/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.scss']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() orders: Order[];
+  @ViewChild('modal') modalRef: ElementRef;
+
+  modal: MaterialInstance;
+  selectedOrder: Order;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(){
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy(){
+    this.modal.destroy();
+  }
+
+  closeModal() {
+    this.modal.close();
+  }
+
+  computerPrice( order: Order ): number {
+    return order.list.reduce(( total, item ) => {
+      return total += item.cost * item.quantity;
+    }, 0 );
+  }
+
+  selectOrder(order: Order) {
+    this.selectedOrder = order;
+    this.modal.open();
   }
 
 }
